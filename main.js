ad03f397e3345ea5f0d338f67226f0d388dcc6b3
Justin David Hofmann <justindhofmann@gmail.com>
2019-06-30 15:17:34 -0400

    updated Checkpoint items

---
+++ main.js
@@ -0,0 +1,5 @@
+// label class could be blank for question format, and toggled correct and incorrect for answer page.
+
+// prompt after question can be the same for either correct or incorrect, with the exception of CORRECT or INCORRECT at the top
+
+// could attempt a huge INCORRECT stamp that covers the answers, maybe on a tilt
