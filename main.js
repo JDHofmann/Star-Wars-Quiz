55a75747f8f6e2a9c36f98d14f052beb374235e0
Justin David Hofmann <justindhofmann@gmail.com>
2019-07-04 13:00:03 -0400

    added real js

--- main.js
+++ main.js
@@ -1,5 +1,126 @@
-// label class could be blank for question format, and toggled correct and incorrect for answer page.
+// ** label class could be blank for question format, and toggled correct and incorrect for answer page.
 
-// prompt after question can be the same for either correct or incorrect, with the exception of CORRECT or INCORRECT at the top
+// ** prompt after question can be the same for either correct or incorrect, with the exception of CORRECT or INCORRECT at the top
 
-// could attempt a huge INCORRECT stamp that covers the answers, maybe on a tilt
+// this function will run when the "start the quiz" btn is clicked via $('.submit-button').on('click', function(){})
+/*
+function generateQuestion(){
+  console.log("`generateQuestion` ran");
+}
+*/
+let questionNumber = 0;
+let currentScore = 0;
+
+function startQuiz() {
+  $('.Questions-Answers').on('click', '.start-quiz', function(event){
+    event.preventDefault();
+    $('.start-quiz').css('display', 'none');
+    $('.shouldWeStart').css('display', 'none');
+    $('.test-tracker').append(`
+      <li>Question: ${questionNumber}/10</li><li>Score: ${currentScore}</li>`);
+    console.log('`startQuiz` ran')
+    createQuestion();
+
+  }
+  );
+}
+
+function createQuestion() {
+  $('.Question').css('display', 'block');
+  $('form').css('display', 'block');
+
+  $('.Question').text(`${STORE[0].question}`);
+
+  $('form').html(`<fieldset>
+    <label class="answer-option">
+      <input type="radio" name="answer" value="${STORE[0].answers[0]}">
+    <span>${STORE[0].answers[0]}</span>
+    </label>
+    <label class="answer-option">
+      <input type="radio" name="answer" value="${STORE[0].answers[1]}">
+    <span>${STORE[0].answers[1]}</span>
+    </label>
+    <label class="answer-option">
+      <input type="radio" name="answer" value="${STORE[0].answers[2]}">
+    <span>${STORE[0].answers[2]}</span>
+    </label>
+    <label class="answer-option">
+      <input type="radio" name="answer" value="${STORE[0].answers[3]}">
+    <span>${STORE[0].answers[3]}</span>
+    </label>
+    <button type="submit" class="submit-button" name="button">Submit</button>
+  </fieldset>`);
+  console.log('`createQuestion` ran');
+
+}
+
+
+// target form on click of submit button
+ $('form').on('click', '.submit-button', function(event) {
+   event.preventDefault();
+   checkAnswer();
+   selectedAnswer();
+ })
+
+function checkAnswer() {
+  // check to see is value of answer === STORE[0].correctAnswer
+}
+
+function selectedAnswer() {
+  console.log('`selectedAnswer` ran');
+  $('.submit-button').css('display', 'none');
+  $('.nextQuestion').css('display', 'block');
+
+  // target selected answer - if else on is it correct
+  // if correct answer choosen {
+  //  turn correct answer green
+  //  display h3 .correctOrIncorrect CORRECT
+  //  }
+  // else {}
+  // display trivia in h2
+
+  // update Score in testTracker
+}
+function createNextQuestion() {
+  $('form').on('click', 'nextQuestion', function() {
+
+  })
+  // update Question in test tracker
+
+  // create a next question button to replace Submit
+  // each time it is clicked we run this function
+  // this function will be a loop with a function exactly the
+  // same as generateQuestion, but with "i" starting on 1, for STORE[i]
+
+  console.log('`createNextQuestion` ran');
+}
+
+function createQuiz() {
+  startQuiz();
+  // createQuestion();
+  // selectedAnswer();
+  createNextQuestion();
+}
+$(createQuiz);
+// render the page
+
+// go to question 1 when clicked
+
+// display which question Number and Score
+// Change submit button text to Submit
+// input questions and answers in H2 and labels for each question
+
+// when submit button clicked:
+// opacity .3 for answers not selected
+// highlight green correct answer
+// display correct or incorrect
+// show additional trivia along with question
+// change submit button text to Next question
+
+// ** repeat until 10/10 questions answered
+
+// display final Score and if the user is a Jedi master or not
+// change submit button text to Play Again
+
+// add hover features for each answer
+// invert submit button color for hover
---
+++ store.js
@@ -0,0 +1,79 @@
+STORE = [
+  {
+    question: "Who are the only two characters who appear in every Star Wars movie?",
+    answers: [
+      "Han and Chewbacca", "Darth Vader and Emperor Palpatine", "R2-D2 and C-3PO", "Luke and Leia"
+    ],
+    correctAnswer: "R2-D2 and C-3PO",
+    trivia: "The first Star Wars movie  R2-D2 and C-3PO do not appear in is Solo. they even have cameos in many non-Star Wars films"
+  },
+  {
+    question: "In which movie does Luke find out Darth Vader is his father?",
+    answers: [
+      "A New Hope", "Return of the Jedi", "Empire Strikes Back", "Attack of the Clones"
+    ],
+    correctAnswer: "Return of the Jedi",
+    trivia: 'Darth Vader never actually says “Luke, I am your father.” He actually says, “No, I am your father.”'
+  },
+  {
+    question: "What is Kylo Ren’s real name?",
+    answers: [
+      "Ben Solo","Han Solo","Rey Solo","Luke Solo"],
+    correctAnswer: "Ben Solo",
+    trivia: "Ben Solo is the son of Leia Organa and Han Solo, renaming himself Kylo Ren after turning to the dark side of the Force."
+  },
+  {
+    question: "Who built C-3PO?",
+    answers: [
+      "Obi One Kenobi", "Rey", "Luke Skywalker", "Anakin Skywalker"
+    ],
+    correctAnswer: "Anakin Skywalker",
+    trivia: "When we first meet a young Anakin Skywalker on Tatooine, he excitedly shows off an unfinished droid, he calls '3PO' to Padme."
+  },
+  {
+    question: "Who killed Qui-Gon Jinn?",
+    answers: [
+      "Darth Sidious", "Anakin Skywalker", "Jango Fett", "Darth Maul"],
+    correctAnswer: "Darth Maul",
+    trivia: "Darth Maul kills Qui-Gon Jinn while Obi One Kenobi is trapped behind a forcefield and unable to help."
+  },
+  {
+    question: "Who inherits Han Solo’s ship?",
+    answers: ["Finn","Poe Dameron","Rey","Chewbacca"],
+    correctAnswer: "Rey",
+    trivia: "After Han Solo’s death, Chewbacca recognizes Rey as the rightful heir to the ship."
+  },
+  {
+    question: "Who is Luke and Leia’s mother?",
+    answers: [
+      "Breha Organa", "Beru Lars", "Cade Skywalker", "Padmé Amidala"
+    ],
+    correctAnswer: "Padmé Amidala",
+    trivia: "Luke and Leia finally find out they are twins in Return of the Jedi, when it is revealed that they were separated at birth."
+  },
+  {
+    question: "What is Luke Skywalker’s homeworld?",
+    answers: [
+      "Tatooine", "Hoth", "Jakku", "Naboo"
+    ],
+    correctAnswer: "Tatooine",
+    trivia: "Obi One Kenobi ironically hid Luke on Tatooine, Anakin's home planet, to keep him safe from Darth Vader."
+  },
+  {
+    question: "What position does Padmé Amidala hold following her term as Queen of Naboo?",
+    answers: [
+      "President of the Trade Federation", "Senator", "Sith Lord", "Supreme Chancellor"
+    ],
+    correctAnswer: "Senator",
+    trivia: "As a senator Padme oversaw the last few years of the Galactic Republic before the Empire took control."
+  },
+  {
+    question: "Who is Princess Leia’s only hope?",
+    answers: [
+      "Luke Skywalker", "Obi One Kenobi", "R2 D2", "Han Solo"
+    ],
+    correctAnswer: "Obi One Kenobi",
+    trivia: "As one of the few remaining Jedi, and the watcher of Luke, Leia was always told her to contact Obi One on Tatooine, if her troubles ever became desperate."
+  }
+
+];
