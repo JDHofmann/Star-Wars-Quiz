a99a84974801f75249bca12870bba829702422d8
Justin David Hofmann <justindhofmann@gmail.com>
2019-07-06 15:25:22 -0400

    implemented js questions

--- main.js
+++ main.js
@@ -10,56 +10,84 @@ function generateQuestion(){
 */
 let questionNumber = 0;
 let currentScore = 0;
+/*
+function updateQuestionNumber(){
+  questionNumber++;
+  console.log('`updateQuestionNumber` just ran');
+  console.log(`${questionNumber}`);
+}
+*/
+function changeQuestionNumber() {
+  //if (questionNumber < STORE.length) {
+    questionNumber ++;
+  //}
+  $('.question-number').text(questionNumber+1);
+}
 
 function updateScore() {
-  currentScore ++;
+  currentScore = currentScore + 1;
+}
+function renderScore() {
+  $('.score').text(currentScore);
+}
+/*
+function renderTestTracker() {
   $('.test-tracker').html(`
     <li>Question: ${questionNumber}/10</li><li>Score: ${currentScore}</li>`);
     console.log(currentScore);
 }
-
+*/
 function startQuiz() {
   $('.Questions-Answers').on('click', '.start-quiz', function(event){
     event.preventDefault();
     $('.start-quiz').css('display', 'none');
     $('.shouldWeStart').css('display', 'none');
-    $('.test-tracker').append(`
-      <li>Question: ${questionNumber}/10</li><li>Score: ${currentScore}</li>`);
+    //renderTestTracker();
     console.log('`startQuiz` ran')
-    createQuestion();
-
+    questionGenerator();
   }
   );
 }
 // turn into a if else loop
-function createQuestion() {
-  $('.Question').css('display', 'block');
-  $('form').css('display', 'block');
+function questionGenerator() {
+  $('.nextQuestion').css('display', 'none');
+  $('.correctOrIncorrect').css('display', 'none');
+  $('.correctedAnswer').css('display', 'none');
+  $('.trivia').css('display', 'none');
 
-  $('.Question').text(`${STORE[0].question}`);
 
-  $('form').html(`<fieldset>
-    <label class="answer-option">
-      <input type="radio" name="answer" value="${STORE[0].answers[0]}">
-    <span>${STORE[0].answers[0]}</span>
-    </label>
-    <label class="answer-option">
-      <input type="radio" name="answer" value="${STORE[0].answers[1]}">
-    <span>${STORE[0].answers[1]}</span>
-    </label>
-    <label class="answer-option">
-      <input type="radio" name="answer" value="${STORE[0].answers[2]}">
-    <span>${STORE[0].answers[2]}</span>
-    </label>
-    <label class="answer-option">
-      <input type="radio" name="answer" value="${STORE[0].answers[3]}">
-    <span>${STORE[0].answers[3]}</span>
-    </label>
-    <button type="submit" class="submit-button" name="button">Submit</button>
-  </fieldset>`);
+
+  if ( questionNumber < STORE.length){
+    renderScore();
+    $('.Question').css('display', 'block');
+    $('form').css('display', 'block');
+
+    $('.Question').text(`${STORE[questionNumber].question}`);
+
+    $('form').html(`<fieldset>
+      <label class="answer-option a">
+        <input type="radio" name="answer" value="${STORE[questionNumber].answers[0]}">
+      <span>${STORE[questionNumber].answers[0]}</span>
+      </label>
+      <label class="answer-option b">
+        <input type="radio" name="answer" value="${STORE[questionNumber].answers[1]}">
+      <span>${STORE[questionNumber].answers[1]}</span>
+      </label>
+      <label class="answer-option c">
+        <input type="radio" name="answer" value="${STORE[questionNumber].answers[2]}">
+      <span>${STORE[questionNumber].answers[2]}</span>
+      </label>
+      <label class="answer-option d">
+        <input type="radio" name="answer" value="${STORE[questionNumber].answers[3]}">
+      <span>${STORE[questionNumber].answers[3]}</span>
+      </label>
+      <button type="submit" class="submit-button" name="button">Submit</button>
+    </fieldset>`);
+  }
+
   console.log('`createQuestion` ran');
-
 }
+
 
 
 // target form on click of submit button
@@ -68,6 +96,7 @@ let answerSelected = '';
    event.preventDefault();
     answerSelected = $( "input:checked" ).val();
    $('.Question').css('display', 'none');
+
    //if (answerSelected !=== ''){
      checkAnswer();
      selectedAnswer();
@@ -79,7 +108,7 @@ let answerSelected = '';
  })
 
 function triviaDisplay() {
-  $('.trivia').text(STORE[0].trivia);
+  $('.trivia').text(STORE[questionNumber].trivia);
   $('.trivia').css('display', 'block');
 }
 let currentAnswerCorrectOrIncorrect = '';
@@ -87,23 +116,42 @@ let currentAnswerCorrectOrIncorrect = '';
 function checkAnswer() {
   // check to see is value of answer === STORE[0].correctAnswer
   $('.correctOrIncorrect').css('display', 'block');
-  if ( answerSelected === STORE[0].correctAnswer){
-    currentAnswerCorrectOrIncorrect = 'correct';
-    $('.correctOrIncorrect').css('color', 'green');
+  if ( answerSelected === STORE[questionNumber].correctAnswer){
+    currentAnswerCorrectOrIncorrect = 'correct!';
+    let selectedAnswerLabel = $( "input:checked" ).closest('label');
+    $('.correctOrIncorrect').css('color', '#80E2AD');
+    $(selectedAnswerLabel).addClass('correct');
     updateScore();
   }
   else {
-    currentAnswerCorrectOrIncorrect = 'incorrect';
+    currentAnswerCorrectOrIncorrect = `incorrect`;
+    $('.correctedAnswer').text(`the correct answer is ${STORE[questionNumber].correctAnswer}`);
     $('.correctOrIncorrect').css('color', 'red');
   }
   $('.correctOrIncorrect').text(currentAnswerCorrectOrIncorrect);
-  
+
+}
+
+function renderSolution() {
+
+  let selectedAnswerLabel = $( "input:checked" ).closest('label');
+  $(selectedAnswerLabel).removeClass('answer-option');
+  $(selectedAnswerLabel).addClass('selected');
+  $('.answer-option').css('opacity', '.3');
+
+// find label of correctAnswer to turn it green
+
+  renderScore();
 }
 
 function selectedAnswer() {
   console.log('`selectedAnswer` ran');
   $('.submit-button').css('display', 'none');
   $('.nextQuestion').css('display', 'block');
+  renderSolution();
+  createNextQuestion();
+
+  // make selected answer stay opacity 1
 
   // target selected answer - if else on is it correct
   // if correct answer choosen {
@@ -116,24 +164,27 @@ function selectedAnswer() {
   // update Score in testTracker
 }
 function createNextQuestion() {
-  $('form').on('click', 'nextQuestion', function() {
+  // $('.Questions-Answers').css('background-color', 'black');
+  $('.nextQuestion').click(function(event){
+    console.log('`createNextQuestion` is running');
+    changeQuestionNumber()
+    // updateQuestionNumber();
+    questionGenerator();
+  });
 
-  })
-  // update Question in test tracker
-
-  // create a next question button to replace Submit
-  // each time it is clicked we run this function
-  // this function will be a loop with a function exactly the
-  // same as generateQuestion, but with "i" starting on 1, for STORE[i]
-
-  console.log('`createNextQuestion` ran');
 }
+// update Question in test tracker
+
+// create a next question button to replace Submit
+// each time it is clicked we run this function
+// this function will be a loop with a function exactly the
+// same as generateQuestion, but with "i" starting on 1, for STORE[i]
 
 function createQuiz() {
   startQuiz();
-  // createQuestion();
+  //createQuestion();
   // selectedAnswer();
-  createNextQuestion();
+  // createNextQuestion();
 }
 $(createQuiz);
 // render the page
